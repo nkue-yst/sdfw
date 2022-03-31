2ca7b766a10e6b65f91537c7801816d9723da481
nkue-yst <39930174+nkue-yst@users.noreply.github.com>
2022-03-31 22:31:05 +0900

    Add sample of ping-pong

---
+++ sample/source/Sample_PingPong.cpp
@@ -0,0 +1,118 @@
+#include <sdfw.h>
+#include <random>
+#include <string>
+
+#define WIDTH 1280
+#define HEIGHT 720
+
+using namespace sdfw;
+
+class Paddle
+{
+public:
+    Paddle()
+        : x(0)
+        , y(HEIGHT / 2 - 100)
+    {
+    }
+
+    void draw()
+    {
+        Line(this->x, this->y, this->x, this->y + 200, 10).draw();
+    }
+
+    uint32_t x, y;
+};
+
+class Ball
+{
+public:
+    Ball()
+        : x(WIDTH / 2)
+        , y(HEIGHT / 2)
+        , dx(2)
+        , dy(2)
+    {
+    }
+
+    void draw()
+    {
+        Circle(this->x, this->y, 10).draw();
+    }
+
+    uint32_t x, y, dx, dy;
+};
+
+int main()
+{
+    std::random_device rnd;
+    Paddle player, npc;
+    Ball ball;
+    uint32_t score_player = 0;
+    uint32_t score_npc = 0;
+
+    player.x = 0 + 100;
+    npc.x = WIDTH - 100;
+
+    init();
+    openWindow(WIDTH, HEIGHT);
+    Scene::setBackground(Color(0, 0, 0));
+
+    while (System::update())
+    {
+        // Score
+        print(std::to_string(score_player) + " - " + std::to_string(score_npc));
+
+        // Player's paddle movement
+        player.y = Mouse::pos().y - 100;
+
+        // NPC's paddle movement
+        npc.y = ball.y - 100 + 80;
+
+        // Update X (Judge goal)
+        if (ball.x + ball.dx >= WIDTH)
+        {
+            score_player++;
+            ball.x = WIDTH / 2;
+            ball.y = HEIGHT / 2;
+        }
+        else if (ball.x + ball.dx <= 0)
+        {
+            score_npc++;
+            ball.x = WIDTH / 2;
+            ball.y = HEIGHT / 2;
+        }
+        else
+        {
+            ball.x += ball.dx;
+        }
+
+        // Update Y
+        if (ball.y + ball.dy >= HEIGHT)
+        {
+            ball.dy *= -1;
+        }
+        else
+        {
+            ball.y += ball.dy;
+        }
+
+        // Player's paddle reflection
+        if (player.x <= ball.x - 5 && ball.x - 5 <= player.x + 5 && player.y <= ball.y && ball.y <= player.y + 200)
+        {
+            ball.dx *= -1;
+        }
+        
+        // NPC's paddle reflection
+        if (npc.x - 5 <= ball.x + 5 && ball.x + 5 <= npc.x + 5 && npc.y <= ball.y && ball.y <= npc.y + 200)
+        {
+            ball.dx *= -1;
+        }
+
+        ball.draw();
+        player.draw();
+        npc.draw();
+    }
+
+    quit();
+}
